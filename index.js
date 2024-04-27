077b6c55dc632a42b0b74ff3fe1190164d6491c3
Ali El-Zayat <alielzayat07@gmail.com>
2024-04-27 22:44:30 +0300

    vercel

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api", authRouter);
 app.use("/api", productsRouter);
 
